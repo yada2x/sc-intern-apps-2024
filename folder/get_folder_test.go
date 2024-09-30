d4dfda679f827ab25b195431fe03f8d212f38fa9
yada2x <lawrenceltnguyen@gmail.com>
2024-10-01 00:14:11 +1000

    Add one test for get_folder methods

--- folder/get_folder_test.go
+++ folder/get_folder_test.go
@@ -5,24 +5,37 @@ import (
 
 	"github.com/georgechieng-sc/interns-2022/folder"
 	"github.com/gofrs/uuid"
-	// "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
 )
 
 // feel free to change how the unit test is structured
 func Test_folder_GetFoldersByOrgID(t *testing.T) {
 	t.Parallel()
+
+	defaultOrdID := uuid.FromStringOrNil(folder.DefaultOrgID)
+
 	tests := [...]struct {
 		name    string
 		orgID   uuid.UUID
 		folders []folder.Folder
 		want    []folder.Folder
 	}{
-		// TODO: your tests here
+		{
+			name: "One folder",
+			orgID: defaultOrdID,
+			folders: []folder.Folder{
+				{Name: "alpha", OrgId: defaultOrdID, Paths: "alpha"},
+			},
+			want: []folder.Folder{
+				{Name: "alpha", OrgId: defaultOrdID, Paths: "alpha"},
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			// f := folder.NewDriver(tt.folders)
-			// get := f.GetFoldersByOrgID(tt.orgID)
+			f := folder.NewDriver(tt.folders)
+			get := f.GetFoldersByOrgID(tt.orgID)
+			assert.Equal(t, tt.want, get)
 
 		})
 	}
