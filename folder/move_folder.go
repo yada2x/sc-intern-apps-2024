8e52677b30c8ef811a5816365ceb16dbda31a192
Lawrence Nguyen <lawrenceltnguyen@gmail.com>
2024-10-01 05:04:29 +0000

    Create scaffold for move_folder method and unit testing

--- folder/move_folder.go
+++ folder/move_folder.go
@@ -1,7 +1,55 @@
 package folder
 
+import (
+	"errors"
+
+	"strings"
+)
+
 func (f *driver) MoveFolder(name string, dst string) ([]Folder, error) {
 	// Your code here...
+	// find node to move, find destination
+	// get old path of node, change it to destination path + "." + node.name
+	// for children nodes with prefix old path, change to new path + "." + child.name
+	folders := f.folders
 
-	return []Folder{}, nil
+	if len(folders) == 0 {
+		return nil, errors.New("provided folder is empty")
+	}
+
+	var start int
+	var dest int
+	for i := range folders {
+		if folders[i].Name == name {
+			start = i
+		} else if folders[i].Name == dst {
+			dest = i
+		}
+	}
+
+	nodeToMove := folders[start]
+	destination := folders[dest]
+
+	if nodeToMove.Name == "" {
+		return nil, errors.New("invalid start node")
+	} else if destination.Name == "" {
+		return nil, errors.New("invalid destination node")
+	} else if nodeToMove.OrgId != destination.OrgId {
+		return nil, errors.New("start and destination nodes belong to different organisations")
+	} else if strings.HasPrefix(destination.Paths, nodeToMove.Paths) {
+		return nil, errors.New("cannot move node to its child")
+	}
+	
+	newPath := destination.Paths + "." + nodeToMove.Name
+	prefix := nodeToMove.Paths + "."
+	nodeToMove.Paths = newPath
+
+	for i := range folders {
+		if strings.HasPrefix(folders[i].Paths, prefix) {
+			leftover := strings.TrimPrefix(folders[i].Paths, prefix)
+			folders[i].Paths = newPath + "." + leftover
+		}
+	}
+
+	return folders, nil
 }
--- folder/move_folder_test.go
+++ folder/move_folder_test.go
@@ -2,8 +2,73 @@ package folder_test
 
 import (
 	"testing"
+	"fmt"
+
+	"github.com/georgechieng-sc/interns-2022/folder"
+	"github.com/gofrs/uuid"
+	// "github.com/stretchr/testify/assert"
 )
 
 func Test_folder_MoveFolder(t *testing.T) {
-	// TODO: your tests here
+	t.Parallel()
+
+	defaultOrdID := uuid.FromStringOrNil(folder.DefaultOrgID)
+	secondaryOrdID := uuid.Must(uuid.NewV4())
+
+	example1 := []folder.Folder{
+		{ Name: "alpha", Paths: "alpha", OrgId: defaultOrdID },
+		{ Name: "bravo", Paths: "alpha.bravo", OrgId: defaultOrdID },
+		{ Name: "charlie", Paths : "alpha.bravo.charlie", OrgId: defaultOrdID },
+		{ Name: "delta", Paths: "alpha.delta", OrgId: defaultOrdID },
+		{ Name: "echo", Paths: "alpha.delta.echo", OrgId: defaultOrdID },
+		{ Name: "foxtrot", Paths: "foxtrot", OrgId: secondaryOrdID},
+		{ Name: "golf", Paths: "golf", OrgId: defaultOrdID },
+	}
+
+	tests := [...]struct {
+		testName    string
+		start		string
+		destination	string
+		orgID   uuid.UUID
+		folders []folder.Folder
+		want    []folder.Folder
+	}{
+		{
+			testName: "Empty folder",
+			start: "bravo",
+			destination: "delta",
+			orgID: defaultOrdID,
+			folders: example1,
+			want: []folder.Folder{
+				{ Name: "alpha", Paths: "alpha", OrgId: defaultOrdID },
+				{ Name: "bravo", Paths: "golf.bravo", OrgId: defaultOrdID },
+				{ Name: "charlie", Paths: "golf.bravo.charlie", OrgId: defaultOrdID },
+				{ Name: "delta", Paths: "alpha.delta", OrgId: defaultOrdID },
+				{ Name: "echo", Paths: "alpha.delta.echo", OrgId: defaultOrdID },
+				{ Name: "foxtrot", Paths: "foxtrot", OrgId: secondaryOrdID },
+				{ Name: "golf", Paths: "golf", OrgId: defaultOrdID },
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.testName, func(t *testing.T) {
+			f := folder.NewDriver(tt.folders)
+			get, _ := f.MoveFolder(tt.start, tt.destination)
+
+			fmt.Println("INPUT")
+			for _, folder := range tt.folders {
+				str := folder.Name + " | " + folder.Paths + " | "+ folder.OrgId.String() 
+				fmt.Println(str)
+			}
+
+			fmt.Println("GOT")
+			for _, folder := range get {
+				str := folder.Name + " | " + folder.Paths + " | "+ folder.OrgId.String() 
+				fmt.Println(str)
+			}
+
+			// assert.Equal(t, tt.want, get)
+		})
+	}
 }
